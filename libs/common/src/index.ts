c7b68351716f7933c4678521f7be39e745285008
Danila Skripko <dalas.prog@gmail.com>
2023-04-08 09:53:12 +0400

    Create database module

--- libs/common/src/index.ts
+++ libs/common/src/index.ts
@@ -1,2 +1,3 @@
-export * from './common.module';
-export * from './common.service';
+export * from './database/database.module';
+export * from './rmq/rmq.service';
+export * from './rmq/rmq.module';
